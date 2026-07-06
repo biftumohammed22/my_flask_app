080c795566a1706bbf99e61d7e6dd588392719f7
Biftu Mohammed <biftumohammed22@gmail.com>
2026-07-06 19:25:13 +0000

    Add webhook for auto deployment

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+import git
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, Flask app is running!"
+
+
+@app.route("/update_server", methods=["POST"])
+def webhook():
+    if request.method == "POST":
+        repo = git.Repo("/home/myflaskapp1/my_flask_app")
+        origin = repo.remotes.origin
+        origin.pull()
+        return "Updated PythonAnywhere successfully", 200
+    else:
+        return "Wrong event type", 400
+
 
 if __name__ == "__main__":
     app.run()
